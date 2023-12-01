4fdc85f1441451cbc005a0872c9a566c55be2585
Muhammad Aziz Prasetyo <azizprasetyo100901@gmail.com>
2023-12-02 02:15:48 +0700

    Create a web server.

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import figlet from "figlet";
+
+const PORT = 3000
+
+const server = Bun.serve({
+  port: PORT,
+  fetch(req) {
+    const resBody = figlet.textSync("My name is Prasetyo")
+
+    return new Response(resBody)
+  }
+})
+
+console.log(`Application starting on http://localhost:${PORT}`)
