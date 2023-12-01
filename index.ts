8398c0404f7d0cc1bbe2ced1ac3a231f34e29d20
Muhammad Aziz Prasetyo <azizprasetyo100901@gmail.com>
2023-12-02 02:33:31 +0700

    Create routes: /, /about, /project, /contact, and /squel.

--- index.ts
+++ index.ts
@@ -5,9 +5,36 @@ const PORT = 3000
 const server = Bun.serve({
   port: PORT,
   fetch(req) {
-    const resBody = figlet.textSync("My name is Prasetyo")
+    const url = new URL(req.url)
 
-    return new Response(resBody)
+    if (url.pathname === '/') {
+      return new Response(figlet.textSync("Home"))
+    }
+
+    if (url.pathname === '/about') {
+      return new Response(figlet.textSync("About Me"))
+    }
+
+    if (url.pathname === '/project') {
+      return new Response(figlet.textSync("My Project"))
+    }
+
+    if (url.pathname === '/contact') {
+      return new Response(figlet.textSync("Contact Us"))
+    }
+
+    if (url.pathname === '/squel') {
+      throw new Error("Could not fetch \"SQUEL\"")
+    }
+
+    return new Response(figlet.textSync("Not Found 404"))
+  },
+  error(err) {
+    return new Response(`<pre>${err}\n${err.stack}</pre>`, {
+      headers: {
+        'Content-Type': 'text/html'
+      }
+    })
   }
 })
 
